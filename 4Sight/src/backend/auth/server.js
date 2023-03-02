b47b2dcb59b017ca264d19f4e8bda06fb6b67838
xulaing <khauv@live.fr>
2023-03-03 00:20:59 +0100

    add api and auth

---
+++ 4Sight/src/backend/auth/server.js
@@ -0,0 +1,251 @@
+const express = require("express");
+const app = express();
+const { pool } = require("./dbConfig");
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+var alert = require('alert');
+
+const initializePassport = require("./passportConfig");
+
+initializePassport(passport);
+
+const PORT = process.env.PORT || 8085;
+app.use("/css", express.static("css"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.set("view engine", "ejs");
+
+app.use('/img', express.static(__dirname + '/assets'));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register");
+});
+
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  res.render("login");
+});
+
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+  res.render("dashboard", { user: req.user.name });
+});
+
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  res.render("index", { message: "You have logged out successfully" });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { username, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  if (!username || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password != password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    // Form validation has passed
+    pool.query(
+      `select * from users where email = $1`,
+      [email],
+      (err, response) => {
+        if (err) {
+          throw err;
+        }
+        if (response.rows.length > 0) {
+          errors.push({ message: "User already registered" });
+          res.render("register", { errors });
+        } else {
+          pool.query(
+            `insert into users(name, email, password, level) values ($1, $2, $3, $4) RETURNING id, password`,
+            [username, email, hashedPassword, 1],
+            (err, response) => {
+              if (err) {
+                throw err;
+              }
+              req.flash("sucess_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+// Connection sécurisée
+// app.post(
+//   "/users/login",
+//   passport.authenticate("local", {
+//     failureFlash: true
+//   }), (req, res) => {
+//     const errorMessage = req.flash('error')[0];
+//     if (errorMessage) {
+//       res.status(401).json({ success: false, message: errorMessage });
+//     } else {
+//       res.json({ success: true });
+//     }
+//   }
+// );
+
+app.post("/users/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // Send custom error message in response
+      return res.status(401).json({ message: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.json({ success: true });
+    });
+  })(req, res, next);
+});
+
+/*
+// Connection non sécurisée
+app.post("/users/login", function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var query =
+    "SELECT name FROM users where email = '" + email + "' and password = '" + password + "'";
+
+  console.log("email: " + email);
+  console.log("password: " + password);
+  console.log("query: " + query);
+
+  pool.query(query, (err, response) => {
+    if (err) {
+      console.log('here');
+      throw err;
+    } else if (response.rowCount == 0) {
+      res.redirect("/users/login");
+    } else {
+      const user = response.rows[0];
+      res.render("dashboard", { user: user.name });
+    }
+  });
+});*/
+
+app.get("/users/administrator", checkAdministrator, (req, res) => {
+  var query = "SELECT id, name, email, level from users"
+  pool.query(query, (err, response) => {
+    if (err) {
+      throw err;
+    }
+    else {
+      const users = response.rows;
+      var queryOrder = "SELECT * from order_data join order_product on order_data.order_id = order_product.order_id order by datetime";
+      pool.query(queryOrder, (err, responseOrder) => {
+        if (err) {
+          throw err;
+        }
+        else {
+          const orders = responseOrder.rows;
+          res.render("admin", { users: users, orders: orders, currentUser: req.user })
+        }
+      })
+    }
+  })
+})
+
+app.get("/users/profile", checkNotAuthenticated, (req, res) => {
+  var query = "SELECT id, name, email, level from users"
+  pool.query(query, (err, response) => {
+    if (err) {
+      throw err;
+    } else {
+      res.render("profile", { name: req.user.name, email: req.user.email, currentUser: req.user });
+    }
+  })
+})
+
+app.post("/users/profile", async (req, res) => {
+  let { email, username, password, password2 } = req.body;
+  let name = req.user.name;
+  let errors = [];
+  if (password != password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+  if (errors.length > 0) {
+    res.render("profile", { name, email, errors });
+  }
+  else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    let query = "update users set name='" + username + "', password='" + hashedPassword + "' where email='" + email + "'";
+    pool.query(
+      query,
+      (err, response) => {
+        if (err) {
+          throw err;
+        }
+        req.flash("sucess_msg", "Modifications saved !");
+        res.redirect("/users/profile");
+      });
+  }
+});
+
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
+
+function checkAdministrator(req, res, next) {
+  if (req.isAuthenticated() && (req.user.level === 0)) {
+    return next();
+  } else if (req.isAuthenticated()) {
+    return res.redirect("/users/login");
+  }
+  return res.redirect(403, "/error");
+}
+
+app.listen(PORT, () => {
+  console.log(`Server runing on port ${PORT}`);
+});
---
+++ 4Sight/src/backend/pipelines/pipeline.py
@@ -0,0 +1,57 @@
+from modeling import *
+from data_import import *
+from data_preparation import *
+
+from tensorflow import keras
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+#!#### TO MODIFY #####!#
+
+global db_config
+db_config = {                       #Start server first
+    'host': 'localhost',
+    'database': 'flask_db',
+    'user': 'postgres',
+    'password': 'oui'
+}
+
+train_table_name = 'train_Bearing_Nasa'
+test_table_name = 'test_Bearing_Nasa'
+dow, month = None, None                                                             #! Get from UI
+cat = None                                                                          #! Get from UI
+mult_var = None                                                                     #! Get from UI
+l_periods = None                                                                    #! Get from UI
+
+#*##### Pipeline #####*#
+
+def train_pipeline(train_table_name, db_config, dow=None, month=None, cat=None, mult_var=None, l_periods=None):
+  df = read_table_from_postgres(train_table_name, db_config)
+  df = data_prep(df, dow=dow, month=month, cat=cat, mult_var=mult_var, l_periods=l_periods)
+  if_model = fit_IF(df)
+  df_cleaned = predict_IF(if_model, df)
+  fit_scaler(df_cleaned, train_table_name)
+  df_scaled = apply_scaler(df_cleaned, train_table_name)
+
+  #set_global(df_cleaned.shape[1])
+  ae_fit(df_scaled, train_table_name)
+
+
+def predict_pipeline(train_table_name, test_table_name):
+    df_train = read_table_from_postgres(train_table_name, db_config)
+    df_test = read_table_from_postgres(test_table_name, db_config)
+    df_train = df_train.drop_duplicates(keep='last')
+    df_test = df_test.drop_duplicates(keep='last')
+    df_train = df_train.reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
+
+    df_train_scaled = apply_scaler(df_train, train_table_name)
+    df_test_scaled = apply_scaler(df_test, train_table_name)
+
+    ae_path = "../models/ae_" + str(train_table_name) + ".h5"
+    model = keras.models.load_model(ae_path)
+
+    result = cluster_data(model, df_train_scaled, df_test_scaled, df_test, test_table_name)
+    write_dataframe_to_postgres(result, str(test_table_name) + "_Result", db_config)
